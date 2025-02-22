87662591465d3f0a8a0818913e384be9859f7d31
Jared_loera <salazarloerajared@gmail.com>
2025-02-22 14:06:22 -0600

    views

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -13,19 +13,19 @@ class userController extends Controller
      */
     public function verifyCodeView($email)
     {
-        return view('/Code/verify-code', ['email' => $email]);
+        return view('Code/verify-code', ['email' => $email]);
     }
     public function dashboardView()
     {
-        return view('/Dashboard/index');
+        return view('Dashboard/index');
     }
     public function loginView()
     {
-        return view('/Auth/index');
+        return view('Auth/index');
     }
     public function registerView()
     {
-        return view('/Register/index');
+        return view('Register/index');
     }
     
  
